b44c9559f0c33bfb62e29bd6ae7b2bc93d60d847
Tarun Jakkula <tarunjakkula1236@gmail.com>
2025-05-01 07:25:06 +0000

    Improved validation messages

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -5,34 +5,50 @@ import registerController from "../controllers/authControllers/registerControlle
 import forgotpasswordController from "../controllers/authControllers/forgotpasswordController";
 import resetpasswordController from "../controllers/authControllers/resetpasswordController";
 
+enum FormState {
+  f_name = "First Name",
+  l_name = "Last Name",
+  email = "Email",
+  password = "Password",
+}
+
 // router to handle auth
 const router = Router();
 
-const checkNonEmpty = (field: string) => body(field).trim().notEmpty();
-const checkEmail = () => body("email").trim().notEmpty().isEmail();
+const checkNonEmpty = (field: "f_name" | "l_name") =>
+  body(field)
+    .trim()
+    .notEmpty()
+    .withMessage(`${FormState[field]} must be non-empty`);
+const checkEmail = () =>
+  body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .withMessage("Please enter a valid email address");
 const checkPasswordLength = () =>
-	body("password").trim().notEmpty().isLength({ min: 6 });
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters");
 
+router.post("/login", checkEmail(), checkPasswordLength(), loginController);
 router.post(
-	"/login",
-	checkEmail(),
-	checkPasswordLength(),
-	loginController,
-);
-router.post(
-	"/register",
-	checkEmail(),
-	checkPasswordLength(),
-	checkNonEmpty("f_name"),
-	registerController,
+  "/register",
+  checkEmail(),
+  checkPasswordLength(),
+  checkNonEmpty("f_name"),
+  checkNonEmpty("l_name"),
+  registerController
 );
 router.post("/forgotpassword", checkEmail(), forgotpasswordController);
 router.patch(
-	"/forgotpassword",
-	checkEmail(),
-	checkPasswordLength(),
-	body("code").trim().notEmpty().isLength({ min: 6, max: 6 }),
-	resetpasswordController,
+  "/forgotpassword",
+  checkEmail(),
+  checkPasswordLength(),
+  body("code").trim().notEmpty().isLength({ min: 6, max: 6 }),
+  resetpasswordController
 );
 
 export default router;
--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -7,24 +7,45 @@ import {
 } from "../controllers/profileController/verfiyemailController";
 import updatepasswordController from "../controllers/profileController/updatepasswordController";
 
+enum FormState {
+  _id = "User Id",
+  f_name = "First Name",
+  l_name = "Last Name",
+  old_password = "Current Password",
+  new_password = "New Password",
+}
+
 // router to handle profile
 const router = Router();
 
-const checkNonEmpty = (field: string) => body(field).trim().notEmpty();
+const checkNonEmpty = (field: "_id" | "f_name" | "l_name") =>
+  body(field)
+    .trim()
+    .notEmpty()
+    .withMessage(`${FormState[field]} must be non-empty`);
 const checkPasswordLength = (field: string) =>
-  body(field).trim().notEmpty().isLength({ min: 6 });
+  body(field)
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters");
 
 router.patch(
   "/updatename",
   checkNonEmpty("_id"),
   checkNonEmpty("f_name"),
+  checkNonEmpty("l_name"),
   updatenameController
 );
 router.post("/verifyemail", checkNonEmpty("_id"), sendVerificationEmail);
 router.patch(
   "/verifyemail",
   checkNonEmpty("_id"),
-  body("code").trim().notEmpty().isLength({ min: 6, max: 6 }),
+  body("code")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Invalid code length"),
   verifyEmail
 );
 router.patch(
